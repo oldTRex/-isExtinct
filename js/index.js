b242ce65c0b6ffd147c11a1dcee0367114f3f40d
Mahdi Shahpouri <m.shahpouri@TOLID-02>
2022-05-21 11:48:36 +0430

    my commit

--- js/index.js
+++ js/index.js
@@ -117,3 +117,17 @@ function validate() {
    let msg  = ` welcome ${name}! `;
    alert( msg )
 }
+
+
+const obj1 = {
+  a: 0,
+  b: 2,
+  c: 4
+};
+
+
+const obj2 = Object.assign({c: 4, d: 6}, obj1);
+
+console.log(obj2.c, obj2.d);
+
+
